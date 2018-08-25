fe082061f17ff3e9c6cf4a3214d6281be03bbec4
zhaopeng16 <zhaopeng16@meituan.com>
2018-08-25 19:14:13 +0800

    copy webpack.config.js

---
+++ whole-proj/webpack.config.js
@@ -0,0 +1,39 @@
+const {
+    VueLoaderPlugin
+} = require('vue-loader');
+
+module.exports = {
+    entry: {
+        'b': "./b.js",
+        'tudou': "./tudou.js",
+        'main': './main.js',
+    },
+    output: {
+        path: __dirname,
+        filename: "[name].min.js"
+    },
+    mode: 'development',
+    devtool: '#source-map',
+    module: {
+        rules: [
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
+            { test: /\.vue$/, loader: "vue-loader",
+                options: {
+                    loaders: {
+                        css:'vue-style-loader!css-loader',
+                        js: 'babel-loader'
+                    }
+                }
+            },
+            { test: /\.js$/, loader: "babel-loader" },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                loader: 'file-loader',
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+};
