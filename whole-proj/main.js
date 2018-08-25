c6eed231398be0a5d050b2775508feda5d6b78e3
zhaopeng16 <zhaopeng16@meituan.com>
2018-08-25 20:30:55 +0800

    import vue

--- whole-proj/main.js
+++ whole-proj/main.js
@@ -1,17 +1,23 @@
 console.log(3456);
 let axios = require("axios");
+import App from './app.vue'
+import Vue from 'vue'
 
-axios({
-  method: "get",
-  url: "https://www.apiopen.top/journalismApi"
-}).then(function(response) {
-  var res = response.data.data;
-  console.log(res.auto)
-  var result='';
-  for(var i=0;i<res.auto.length;i++){
-      result+="<div>"+res.auto[i].source+"</div>"
-  }
-  console.log(result)
-
-  document.getElementsByTagName("p")[0].innerHTML = result
-});
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
+//    axios({
+//      method: "get",
+//      url: "https://www.apiopen.top/journalismApi"
+//    }).then(function(response) {
+//      var res = response.data.data;
+//      console.log(res.auto)
+//      var result='';
+//      for(var i=0;i<res.auto.length;i++){
+//          result+="<div>"+res.auto[i].source+"</div>"
+//      }
+//      console.log(result)
+//    
+//      document.getElementsByTagName("p")[0].innerHTML = result
+//    });
