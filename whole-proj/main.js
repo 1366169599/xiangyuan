0af764b6a2c2bc9a848f75be1e9164a4365cb3e6
1366169599 <1366169599D@qq.com>
2018-08-25 20:24:36 +0800

    npm-init

--- whole-proj/main.js
+++ whole-proj/main.js
@@ -1,1 +1,17 @@
 console.log(3456);
+let axios = require("axios");
+
+axios({
+  method: "get",
+  url: "https://www.apiopen.top/journalismApi"
+}).then(function(response) {
+  var res = response.data.data;
+  console.log(res.auto)
+  var result='';
+  for(var i=0;i<res.auto.length;i++){
+      result+="<div>"+res.auto[i].source+"</div>"
+  }
+  console.log(result)
+
+  document.getElementsByTagName("p")[0].innerHTML = result
+});
