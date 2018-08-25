da732c4e6937215f78e7362ee57aef06834d7118
1366169599 <1366169599D@qq.com>
2018-08-25 21:00:17 +0800

    npm-init

--- whole-proj/app.vue
+++ whole-proj/app.vue
@@ -1,5 +1,41 @@
 <template>
     <div class="container">
-        hello world
+        <div v-for='item in news.auto'>
+            {{item.source}}
+            {{item.digest}}
+        </div>
     </div>
 </template>
+
+<script>
+let axios = require("axios");
+
+export default {
+  data() {
+      return {
+          news: {}
+      }
+  },
+  mounted() {
+    axios({
+      method: "get",
+      url: "https://www.apiopen.top/journalismApi"
+    }).then((response) => {
+      this.news = response.data.data;
+      console.log(this.news)
+    //   var result = "";
+    //   for (var i = 0; i < res.auto.length; i++) {
+    //     result += "<div>" + res.auto[i].source + "</div>";
+    //   }
+    //   console.log(result);
+
+    //   document.getElementsByTagName("p")[0].innerHTML = result;
+    });
+  }
+};
+</script>
+
+<style>
+
+</style>
+
--- whole-proj/main.js
+++ whole-proj/main.js
@@ -1,5 +1,4 @@
 console.log(3456);
-let axios = require("axios");
 import App from './app.vue'
 import Vue from 'vue'
 
@@ -7,17 +6,4 @@ new Vue({
     el: '#app',
     render: h => h(App)
 })
-//    axios({
-//      method: "get",
-//      url: "https://www.apiopen.top/journalismApi"
-//    }).then(function(response) {
-//      var res = response.data.data;
-//      console.log(res.auto)
-//      var result='';
-//      for(var i=0;i<res.auto.length;i++){
-//          result+="<div>"+res.auto[i].source+"</div>"
-//      }
-//      console.log(result)
-//    
-//      document.getElementsByTagName("p")[0].innerHTML = result
-//    });
+
